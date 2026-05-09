c2a1ef6bfea733cde89b31e58dc912e27b5253e3
Victor Efimov <victor-efimov>
2026-05-09 16:13:40 -0300

    Creacion de la funcion autoejecutable, arreglo, funcion flecha obtenerProyectos y avances en el Listaproyectos.jsx  [VictorEfimov]

---
+++ my-app/src/components/Listaproyectos.jsx
@@ -0,0 +1,19 @@
+function Listaproyectos() {
+
+    return(
+        <main>
+            <div class="carta">
+                <div class="carta2">
+                    <label>Titulo:</label>
+                </div>
+                <div></div>
+
+    <div class="carta3">
+        <button>Ver detalle</button>
+    </div>
+</div>
+        </main>
+    )
+}
+
+export default Listaproyectos;
